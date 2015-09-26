50b617c6a7334e0120b14a529de7a999b9a136e0
Philip Lowney <lowneyp2@gmail.com>
2015-09-26 21:23:30 +0100

    very simple PoC - facelet, managed bean, DAO EJB, database connection working together to show a table of data

---
+++ src/ui/beans/TableViewUI.java
@@ -0,0 +1,30 @@
+package ui.beans;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import lombok.Getter;
+import lombok.Setter;
+import model.Stand;
+import persistence.dao.StandDAO;
+
+@ManagedBean
+@ViewScoped
+public class TableViewUI
+{
+	@EJB
+	private StandDAO standDAO;
+	
+	@Getter @Setter
+	private List<Stand> stands;
+	
+	@PostConstruct
+	public void init()
+	{
+		stands = standDAO.findAll();
+	}
+}
